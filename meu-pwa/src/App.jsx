f93fb59a49f82155291c575b17792e8cf7c9451d
i6kmn <sarahalcoforadoo@gmail.com>
2026-06-08 21:40:31 +0000

    atualizando o projeto

---
+++ meu-pwa/src/App.jsx
@@ -0,0 +1,8 @@
+
+import AppRoutes from "./routes/AppRoutes";
+import './App.css';
+
+export default function App() {
+  return <AppRoutes />;
+}
+
---
+++ meu-pwa/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import { registerSW } from 'virtual:pwa-register'
+import { BrowserRouter } from 'react-router-dom'
+
+registerSW({ immediate: true })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
